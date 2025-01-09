baf444a1267c2f35a2a977cdbea015f0f5a21508
Jim <thatdeveloperjim@gmail.com>
2025-01-08 22:26:30 -0500

    grid and random color
    grid of squares functional format using flex. not pretty...
    grid squares color change on hover to a random color

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
+// declartions
 const container = document.querySelector("#container");
 
-function createBoxes(boxNum) {
-   for (let i = 0; i < boxNum * boxNum; i++){
-      const row = container.appendChild(document.createElement('div'));
-      for (let j = 0; j < (boxNum - 1); j++){
-         const box = document.createElement('div');
-         box.className='square';
-         row.appendChild(box);
-      }
+
+function getRandomColor() {
+   const letters = '0123456789ABCDEF';
+   let color = '#';
+   for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
    }
+   return color;
 }
-// const gs1 = document.createElement(div);
-// container.appendChild(gs1);
-createBoxes(2);
+
+function createDivs(divNum) {
+   // Loop to create columns
+   for (let i = 0; i < (divNum); i++) {
+      // create new div element
+      const column = document.createElement('div');
+
+      // Add content or attributes to the div
+      column.classList.add("column");
+
+      // Loop for rows
+      for (let j = 1; j <= divNum; j++) {
+         const row = document.createElement('div');
+         row.addEventListener('mouseover', function() {
+            // Add hover effect
+            row.style.backgroundColor = getRandomColor();
+         });
+
+         // Add content or attributes
+         row.classList.add("row");
+         row.style.border = "2px solid black";
+         row.textContent = (i * divNum) + j;
+
+         // Append the rows to columns
+         column.appendChild(row);
+      }
+
+      // Append to the container
+      container.appendChild(column);
+      console.log(container.children.length) //divNum
+
+   }
+
+   // const cells = document.querySelector(".column, .row");
+   // cells.addEventListener('mouseover', function() {
+   //    // Add hover effect
+   //    this.style.backgroundColor = 'blue';
+   // });
+   
+   // cells.addEventListener('mouseover', function() {
+   //    // Remove hover effect
+   //    this.style.backgroundColor = '';
+   // });
+
+
+   
+console.log(divNum);
+}
+//Create x number of divs
+createDivs(16)
